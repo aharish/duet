affa6e1fd168ae2a09bdf97dc4cc65f31173884c
gadgetroid <amoghharish871@gmail.com>
2018-03-12 01:42:23 +0530

    Resolves #12

---
+++ app/src/main/java/com/gadgetroid/duet/helper/BottomNavigationViewHelper.java
@@ -0,0 +1,45 @@
+package com.gadgetroid.duet.helper;
+
+import android.annotation.SuppressLint;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.util.Log;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by gadgetroid on 12/03/18.
+ */
+
+/**
+ * Used for removing the shift on BottomNavigationBar.
+ * Solution taken from this StackOverflow answer:
+ * https://stackoverflow.com/questions/41718633/bottomnavigationview-with-more-than-3-items-tab-title-is-hiding
+ */
+
+
+public class BottomNavigationViewHelper {
+    @SuppressLint("RestrictedApi")
+    public static void removeShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                //noinspection RestrictedApi
+                item.setShiftingMode(false);
+                // set once again checked value, so view will be updated
+                //noinspection RestrictedApi
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+            Log.e("BottomNav", "Unable to get shift mode field", e);
+        } catch (IllegalAccessException e) {
+            Log.e("BottomNav", "Unable to change value of shift mode", e);
+        }
+    }
+}
